96f5ca36cff573280fc50c4840d9d4b0c9b56960
sungar <shai.ungar@cyren.com>
2021-04-27 21:40:46 +0300

    bump version

--- technicolorgateway/__init__.py
+++ technicolorgateway/__init__.py
@@ -11,7 +11,7 @@ from technicolorgateway.modal import get_device_modal, get_broadband_modal
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 class TechnicolorGateway(object):
