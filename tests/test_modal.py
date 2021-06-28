81ea4fae66fa90ba234da6ffd91a71d555c6eeff
Shai Ungar <shaiungar@gmail.com>
2021-06-28 23:31:18 +0300

    add another version that has 8 columns in modal lp

--- tests/test_modal.py
+++ tests/test_modal.py
@@ -14,8 +14,8 @@ class TestModal:
         assert modal_dict['uploaded'] == '898.57'
         assert modal_dict['downloaded'] == '3973.16'
 
-    def test_get_device_modal_gold(self):
-        with open('tests/resources/device-modal_gold.lp') as f:
+    def test_get_device_modal_len6(self):
+        with open('tests/resources/device-modal_len6.lp') as f:
             content = f.read()
         modal_list = get_device_modal(content)
         print('\n')
@@ -28,8 +28,8 @@ class TestModal:
         assert modal_list[-1]['ip'] == '192.168.1.192'
         assert modal_list[-1]['mac'] == '24:62:ab:bb:65:30'
 
-    def test_get_device_modal_aqua(self):
-        with open('tests/resources/device-modal_aqua.lp') as f:
+    def test_get_device_modal_len12(self):
+        with open('tests/resources/device-modal_len_12.lp') as f:
             content = f.read()
         modal_list = get_device_modal(content)
         print('\n')
@@ -41,3 +41,14 @@ class TestModal:
         assert modal_list[-1]['name'] == 'hostname2_aqua'
         assert modal_list[-1]['ip'] == '192.168.1.251'
         assert modal_list[-1]['mac'] == 'fc:8f:81:83:7f:1d'
+
+    def test_get_device_modal_len8(self):
+        with open('tests/resources/device-modal_len_8.lp') as f:
+            content = f.read()
+        modal_list = get_device_modal(content)
+        print('\n')
+        print(modal_list)
+        assert len(modal_list) == 1
+        assert modal_list[0]['name'] == 'Mate'
+        assert modal_list[0]['ip'] == '192.168.1.2'
+        assert modal_list[0]['mac'] == '02:11:12:12:12:12'
