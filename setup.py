49bcee17ade6636ac1cab1cf37b9938f47882191
Shai Ungar <shaiungar@gmail.com>
2021-07-14 12:37:47 +0300

    bump patch version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pytechnicolor",
-    version="1.1.1",
+    version="1.1.2",
     description="Technicolor Gateway library",
     long_description=README,
     long_description_content_type="text/markdown",
