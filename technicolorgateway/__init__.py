a07997c476d0890c87fcb540aff5e6826a13897c
Shai Ungar <shaiungar@gmail.com>
2023-01-05 14:43:01 +0200

    bump patch version 1.1.5

--- technicolorgateway/__init__.py
+++ technicolorgateway/__init__.py
@@ -11,7 +11,7 @@ from technicolorgateway.modal import get_device_modal, get_broadband_modal
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = "1.1.3"
+__version__ = "1.1.5"
 
 
 class TechnicolorGateway(object):
