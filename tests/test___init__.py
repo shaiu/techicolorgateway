04ae7fe5db0e6e0b059db9561eef011a6675dd12
shaiu <shaiungar@gmail.com>
2023-04-14 21:35:42 +0300

    fix test 2

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -10,7 +10,6 @@ def test_srp6authenticate_no_token():
         htm = file.read()
     responses.add(responses.GET, 'http://192.168.1.2:80/', htm, status=200)
     tech_gateway = TechnicolorGateway('192.168.1.2', 80, "admin", "aaaaaa")
-    tech_gateway.authenticate()
     with pytest.raises(TypeError) as err:
         tech_gateway.srp6authenticate()
     assert "'NoneType' object is not subscriptable" in str(err.value)
